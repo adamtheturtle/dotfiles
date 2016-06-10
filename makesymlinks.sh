373a129c8ee134d3484f31c868c4e9145a47af00
Adam Dangoor <adamdangoor@gmail.com>
2016-06-10 17:20:35 +0100

    add tmux conf to script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="config/fish/config.fish config/nvim/init.vim"    # list of files/folders to symlink in homedir
+files="config/fish/config.fish config/nvim/init.vim tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
