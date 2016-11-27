830faf6aea4d83005b0db350d62c03c9dea3a730
Adam Dangoor <adamdangoor@gmail.com>
2016-11-27 14:38:43 +0000

    Fix issue with git push within neovim

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,3 +4,8 @@ set -Ux LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
 alias vim=nvim
 eval (python -m virtualfish)
 set -gx EDITOR vim
+
+# See https://github.com/neovim/neovim/issues/5348
+set -x 'VIM' ''
+set -x 'VIMRUNTIME' ''
+
