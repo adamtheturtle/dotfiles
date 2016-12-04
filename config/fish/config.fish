beecdd77c11bbefda282e4d47a77790fef89fa61
Adam Dangoor <adamdangoor@gmail.com>
2016-12-04 19:52:57 +0000

    Clear virtualfish prompt

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,3 +8,10 @@ set -gx EDITOR vim
 # See https://github.com/neovim/neovim/issues/5348
 set -x 'VIMRUNTIME' ''
 set -x 'VIM' ''
+
+# To get a virtualfish prompt, run:
+# 	`fisher install adamtheturtle/virtualfish-prompt`
+functions -q virtualfish_prompt; and virtualfish_prompt
+
+eval (direnv hook fish)
+clear
