04c5d67132d99b4ee09694c2b8bb2e588c951132
Adam Dangoor <adamdangoor@gmail.com>
2023-03-10 10:34:48 +0000

    Get Homebrew to work in Nova

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,3 @@
-#!/usr/bin/env fish
-
 set -gx GPG_TTY $tty
 set -gx PATH $PATH ~/.local/bin
 set -gx PATH $PATH ~/.cargo/bin
@@ -12,9 +10,20 @@ set -gx EDITOR vim
 set -x 'VIMRUNTIME' ''
 set -x 'VIM' ''
 
-if status --is-interactive
-  eval (/opt/homebrew/bin/brew shellenv)
-end
+# Ideally we would run 'eval (/opt/homebrew/bin/brew shellenv)'
+# However, there is a bad interaction with Nova:
+# https://devforum.nova.app/t/homebrew-fish-nova-errors/2013.
+# Therefore, I have once run 'eval (/opt/homebrew/bin/brew shellenv)'
+# and then moved the result here.
+set -gx HOMEBREW_PREFIX "/opt/homebrew"
+set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar"
+set -gx HOMEBREW_REPOSITORY "/opt/homebrew"
+set -q PATH; or set PATH ''
+set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH
+set -q MANPATH; or set MANPATH ''
+set -gx MANPATH "/opt/homebrew/share/man" $MANPATH
+set -q INFOPATH; or set INFOPATH ''
+set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
