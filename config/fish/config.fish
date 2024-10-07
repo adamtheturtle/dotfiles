c99c8e8777055abc16f928d253c074de800feadc
Adam Dangoor <adamdangoor@gmail.com>
2024-10-07 13:46:53 +0100

    Simplify homebrew shell setup as we are not using nova

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,20 +10,7 @@ set -gx EDITOR vim
 set -x 'VIMRUNTIME' ''
 set -x 'VIM' ''
 
-# Ideally we would run 'eval (/opt/homebrew/bin/brew shellenv)'
-# However, there is a bad interaction with Nova:
-# https://devforum.nova.app/t/homebrew-fish-nova-errors/2013.
-# Therefore, I have once run 'eval (/opt/homebrew/bin/brew shellenv)'
-# and then moved the result here.
-set -gx HOMEBREW_PREFIX "/opt/homebrew"
-set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar"
-set -gx HOMEBREW_REPOSITORY "/opt/homebrew"
-set -q PATH; or set PATH ''
-set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH
-set -q MANPATH; or set MANPATH ''
-set -gx MANPATH "/opt/homebrew/share/man" $MANPATH
-set -q INFOPATH; or set INFOPATH ''
-set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH
+eval (/opt/homebrew/bin/brew shellenv)
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
